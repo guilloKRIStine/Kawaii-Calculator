1ebeb2bd5c66afe7eace6dad576cc8989228c254
Kristina Samokhina <92637527+guilloKRIStine@users.noreply.github.com>
2021-12-13 08:20:14 +0300

    Add files via upload

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.First_box = new System.Windows.Forms.TextBox();
+            this.Third_box = new System.Windows.Forms.TextBox();
+            this.Result_box = new System.Windows.Forms.TextBox();
+            this.Result_button = new System.Windows.Forms.Button();
+            this.Second_box = new System.Windows.Forms.TextBox();
+            this.label_1 = new System.Windows.Forms.Label();
+            this.label_2 = new System.Windows.Forms.Label();
+            this.label_3 = new System.Windows.Forms.Label();
+            this.Info_Box = new System.Windows.Forms.TextBox();
+            this.What_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // First_box
+            // 
+            this.First_box.Location = new System.Drawing.Point(70, 83);
+            this.First_box.Name = "First_box";
+            this.First_box.Size = new System.Drawing.Size(147, 27);
+            this.First_box.TabIndex = 0;
+            this.First_box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.First_Box_KeyPress);
+            // 
+            // Third_box
+            // 
+            this.Third_box.Location = new System.Drawing.Point(70, 182);
+            this.Third_box.Name = "Third_box";
+            this.Third_box.Size = new System.Drawing.Size(147, 27);
+            this.Third_box.TabIndex = 0;
+            this.Third_box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Third_Box_KeyPress);
+            // 
+            // Result_box
+            // 
+            this.Result_box.BackColor = System.Drawing.Color.LavenderBlush;
+            this.Result_box.Cursor = System.Windows.Forms.Cursors.No;
+            this.Result_box.Location = new System.Drawing.Point(22, 34);
+            this.Result_box.Name = "Result_box";
+            this.Result_box.ReadOnly = true;
+            this.Result_box.Size = new System.Drawing.Size(408, 27);
+            this.Result_box.TabIndex = 0;
+            // 
+            // Result_button
+            // 
+            this.Result_button.BackColor = System.Drawing.Color.MistyRose;
+            this.Result_button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Result_button.BackgroundImage")));
+            this.Result_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Result_button.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Result_button.Image = ((System.Drawing.Image)(resources.GetObject("Result_button.Image")));
+            this.Result_button.Location = new System.Drawing.Point(264, 107);
+            this.Result_button.Name = "Result_button";
+            this.Result_button.Size = new System.Drawing.Size(87, 87);
+            this.Result_button.TabIndex = 1;
+            this.Result_button.Text = "OK";
+            this.Result_button.UseVisualStyleBackColor = false;
+            this.Result_button.Click += new System.EventHandler(this.Result_button_Click);
+            // 
+            // Second_box
+            // 
+            this.Second_box.Location = new System.Drawing.Point(70, 135);
+            this.Second_box.MaxLength = 1;
+            this.Second_box.Name = "Second_box";
+            this.Second_box.Size = new System.Drawing.Size(147, 27);
+            this.Second_box.TabIndex = 0;
+            this.Second_box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Second_Box_KeyPress);
+            // 
+            // label_1
+            // 
+            this.label_1.AutoSize = true;
+            this.label_1.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_1.ForeColor = System.Drawing.Color.Black;
+            this.label_1.Location = new System.Drawing.Point(22, 83);
+            this.label_1.MinimumSize = new System.Drawing.Size(27, 27);
+            this.label_1.Name = "label_1";
+            this.label_1.Size = new System.Drawing.Size(27, 27);
+            this.label_1.TabIndex = 2;
+            this.label_1.Text = "1";
+            this.label_1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_2
+            // 
+            this.label_2.AutoSize = true;
+            this.label_2.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_2.ForeColor = System.Drawing.Color.Black;
+            this.label_2.Location = new System.Drawing.Point(22, 135);
+            this.label_2.MinimumSize = new System.Drawing.Size(27, 27);
+            this.label_2.Name = "label_2";
+            this.label_2.Size = new System.Drawing.Size(27, 27);
+            this.label_2.TabIndex = 2;
+            this.label_2.Text = "2";
+            this.label_2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_3
+            // 
+            this.label_3.AutoSize = true;
+            this.label_3.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_3.ForeColor = System.Drawing.Color.Black;
+            this.label_3.Location = new System.Drawing.Point(22, 182);
+            this.label_3.MinimumSize = new System.Drawing.Size(27, 27);
+            this.label_3.Name = "label_3";
+            this.label_3.Size = new System.Drawing.Size(27, 27);
+            this.label_3.TabIndex = 2;
+            this.label_3.Text = "3";
+            this.label_3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Info_Box
+            // 
+            this.Info_Box.BackColor = System.Drawing.Color.LavenderBlush;
+            this.Info_Box.Cursor = System.Windows.Forms.Cursors.No;
+            this.Info_Box.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Info_Box.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Info_Box.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Info_Box.Location = new System.Drawing.Point(22, 235);
+            this.Info_Box.Multiline = true;
+            this.Info_Box.Name = "Info_Box";
+            this.Info_Box.ReadOnly = true;
+            this.Info_Box.Size = new System.Drawing.Size(240, 151);
+            this.Info_Box.TabIndex = 3;
+            // 
+            // What_Button
+            // 
+            this.What_Button.Cursor = System.Windows.Forms.Cursors.Help;
+            this.What_Button.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.What_Button.Location = new System.Drawing.Point(291, 348);
+            this.What_Button.Name = "What_Button";
+            this.What_Button.Size = new System.Drawing.Size(59, 37);
+            this.What_Button.TabIndex = 4;
+            this.What_Button.Text = "?";
+            this.What_Button.UseVisualStyleBackColor = true;
+            this.What_Button.Click += new System.EventHandler(this.What_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.What_Button);
+            this.Controls.Add(this.Info_Box);
+            this.Controls.Add(this.label_3);
+            this.Controls.Add(this.label_2);
+            this.Controls.Add(this.label_1);
+            this.Controls.Add(this.Second_box);
+            this.Controls.Add(this.Result_button);
+            this.Controls.Add(this.Result_box);
+            this.Controls.Add(this.Third_box);
+            this.Controls.Add(this.First_box);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kawaii Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox First_box;
+        private System.Windows.Forms.TextBox Third_box;
+        private System.Windows.Forms.TextBox Result_box;
+        private System.Windows.Forms.Button Result_button;
+        private System.Windows.Forms.TextBox Second_box;
+        private System.Windows.Forms.Label label_1;
+        private System.Windows.Forms.Label label_2;
+        private System.Windows.Forms.Label label_3;
+        private System.Windows.Forms.TextBox Info_Box;
+        private System.Windows.Forms.Button What_Button;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Info_Box.Text = "Привет ♥ Перед тобой kawaii calculator. Чтобы что-то посчитать в 1 и 3 боксе напиши числа, а во втором специалным знаком операцию. Чтобы увидеть список операций нажми на вопросик! ";
+        }
+
+        // ОСНОВА //
+
+        private void Result_button_Click(object sender, EventArgs e)
+        {
+            Info_Box.Clear();
+            Result_box.Clear();
+
+            String Number_First_Box = First_box.Text; // 1 
+            String Solution_Second_Box = Second_box.Text; // 2
+            String Number_Third_Box = Third_box.Text; // 3
+
+            int i = 0;
+            
+            if (Number_First_Box == "" || Solution_Second_Box == "" || Number_Third_Box == "") // проверка что не пустые
+            {
+                Info_Box.Text ="Ой, вы что-то упустили :( Проверьте, чтобы все было введено ♥";
+                i++;
+            }
+
+            // кол-во знаков в первом боксе
+            int NumberPlus1 = Number_First_Box.Where(c => c == '+').Count();
+            int NumberMinus1 = Number_First_Box.Where(c => c == '-').Count();
+            int NumberDots1 = Number_First_Box.Where(c => c == '.').Count();
+            // кол-во знаков во втором боксе
+            int NumberPlus3 = Number_Third_Box.Where(c => c == '+').Count();
+            int NumberMinus3 = Number_Third_Box.Where(c => c == '-').Count();
+            int NumberDots3 = Number_First_Box.Where(c => c == '.').Count();
+
+            if ((NumberPlus1 >= 2) || (NumberMinus1 >= 2) || (NumberDots1 >= 2)  || (NumberPlus3 >= 2) || (NumberMinus3 >= 2) || (NumberDots3 >= 2)) //два раза ++ или -- или ..
+            {
+                Info_Box.Text = "Ой, вы написали лишний знак :( Проверьте, чтобы все было введено правильно ♥";
+                i++;
+            }
+
+            if ((Number_First_Box.Contains("-") && Number_First_Box.Contains("+")) || (Number_Third_Box.Contains("-") && Number_Third_Box.Contains("+"))) // и + и -
+            {
+                Info_Box.Text = "Ой, вы написали лишний знак :( Проверьте, чтобы все было введено правильно ♥";
+                i++;
+            }
+
+            if (Number_First_Box.EndsWith("-") || Number_First_Box.EndsWith("+") || Number_First_Box.EndsWith(".") || Number_Third_Box.EndsWith("-") || Number_Third_Box.EndsWith("+") || Number_Third_Box.EndsWith(".")) // конец на + - .
+            {
+                Info_Box.Text = "Ой, вы написали лишний знак :( Проверьте, чтобы все было введено правильно ♥";
+                i++;
+            }
+
+            if ((Number_First_Box.Length == 1 && (Number_First_Box.Contains("-") || Number_First_Box.Contains("+") || Number_First_Box.Contains("."))) || (Number_Third_Box.Length == 1 && (Number_Third_Box.Contains("-") || Number_Third_Box.Contains("+") || Number_Third_Box.Contains(".")))) // только + и - и .
+            {
+                Info_Box.Text = "Ой, вы написали только знак :( Проверьте, чтобы все было введено правильно ♥";
+                i++;
+            }
+
+
+            // так ну если все ок то погнали считать
+            if (i == 0)
+            {
+                double First = Convert.ToDouble(Number_First_Box);
+                double Third = Convert.ToDouble(Number_Third_Box);
+
+                if (Solution_Second_Box == "+") // сложение
+                {
+                    Result_box.Text = (First + Third).ToString();
+                    Info_Box.Text = "UwU ♥";
+                }
+
+                if (Solution_Second_Box == "-") // вычитание
+                {
+                    Result_box.Text = (First - Third).ToString();
+                    Info_Box.Text = "UwU ♥";
+                }
+
+                if (Solution_Second_Box == "*") // умножение
+                {
+                    Result_box.Text = (First * Third).ToString();
+                    Info_Box.Text = "UwU ♥";
+                }
+
+                if (Solution_Second_Box == "/") // деление
+                {
+                    if (Third != 0)
+                    {
+                        Result_box.Text = (First / Third).ToString();
+                        Info_Box.Text = "UwU ♥";
+                    }
+                    else
+                    {
+                        Info_Box.Text = "Ай ай ай ~ На ноль делить нельзя! Поменяй, пожалуйста ♥";
+                    }
+                }
+            }
+
+        }
+
+        // ПРОВЕРКИ НА ВВОД//
+
+        private void First_Box_KeyPress(object sender, KeyPressEventArgs e) //запрет на ввод букв
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8 && number != 45 && number != 43 && number != 46) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Third_Box_KeyPress(object sender, KeyPressEventArgs e) //запрет на ввод букв
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number) && number != 8 && number!= 45 && number != 43 && number != 46) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Second_Box_KeyPress(object sender, KeyPressEventArgs e) //запрет на ввод букв
+        {
+            char number = e.KeyChar;
+
+            if (number != 8 && number != 45 && number != 43 && number != 47 && number != 42) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        // ДОП ФУНКЦИИ //
+        private void What_Button_Click(object sender, EventArgs e) // вопросик
+        {
+            MessageBox.Show(" Привет ♥ Перед тобой kawaii calculator. Чтобы что-то посчитать в 1 и 3 боксе напиши числа, а во втором специалным знаком операцию. + сложить, - вычесть, * умножить, / поделить, десятичные записывать с точкой", "Информация ♥");
+        }
+    }
+}
